02dc7edc2be4b47893ed2a78f9b77c5fa3a45fa9
Ankur CP <ankur@codeplatoon.org>
2021-11-15 14:52:44 -0600

    lecture update 2 - post lunch

--- book_app/urls.py
+++ book_app/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 urlpatterns = [
     path("", index, name="book_list"),
+    path("new", book_create, name="book_create"),
     path("<int:book_id>", book_view, name="book_detail"),
-    path("new", book_create, name="book_create")
+    path("<int:book_id>/update", book_update, name="book_update"),
+    path("<int:book_id>/delete", book_delete, name="book_delete"),
 ]
--- book_app/views.py
+++ book_app/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from book_app.models import Book
 from book_app.forms import BookForm
 
 def index(request):
     try:
-        all_books = Book.objects.all() # SELECT * FROM book_app_book;
+        all_books = Book.objects.all().order_by("title") # SELECT * FROM book_app_book ORDER BY title
         my_data = {
             "books": all_books,
             "heading": "Hello World"
@@ -31,7 +31,15 @@ def book_view(request, book_id):
 
 def book_create(request):
     try:
-        print(request.method)
+        # GET request
+        if request.method == "GET":
+            form = BookForm()
+            my_data = {
+                "form": form,
+                "action": "Create"
+            }
+            return render(request, "pages/book_form.html", context=my_data)
+
         # POST request
         if request.method == "POST":
             form = BookForm(request.POST)
@@ -40,19 +48,48 @@ def book_create(request):
                 form.save()
             return HttpResponse(f"You added a new book {form.instance.title}")
 
-        # GET request
-        if request.method == "GET":
-            form = BookForm()
-            my_data = {
-                "form": form
-            }
-            return render(request, "pages/book_form.html", context=my_data)
     except Exception as e:
         return HttpResponse("There was an error!!")
 
 
+def book_update(request, book_id):
+    try:
+        # GET request
+        book = Book.objects.get(id=book_id)
+        if request.method == "GET":
+            form = BookForm(instance=book)
+            my_data = {
+                "form": form,
+                "action": "Update"
+            }
+            return render(request, "pages/book_form.html", context=my_data)
 
+        # POST request
+        if request.method == "POST":
+            print(request.POST["title"])
+            print(request.POST.keys())
+            form = BookForm(request.POST, instance=book)
+            if form.is_valid():
+                print("form is valid")
+                form.save()
+            return redirect('book_detail', book_id=book_id)
+
+    except Exception as e:
+        return HttpResponse("There was an error!!")
     
 
+def book_delete(request, book_id):
+    try:
+        # GET request
+        book = Book.objects.get(id=book_id)
+        if request.method == "GET":
+            my_data = {
+                "book": book
+            }
+            return render(request, "pages/book_delete.html", context=my_data)
 
-    
+        if request.method == "POST":
+            Book.objects.get(id=book_id).delete()
+            return redirect("book_list")
+    except Exception as e:
+        return HttpResponse("There was an error!!")
